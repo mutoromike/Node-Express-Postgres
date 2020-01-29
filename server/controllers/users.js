3270fc0dbd74a31534fc6d5121ee5f0b19b350a8
strongestavenger <mikemutoro@gmail.com>
2020-01-29 13:32:20 +0300

    implement register

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,14 +1,20 @@
-const User = require('../models').Users;
+const bcrypt = require("bcrypt");
+const User = require("../models").Users;
+const saltRounds = 10;
 
 module.exports = {
   create(req, res) {
-    return User
-      .create({
-        name: req.body.title,
-        email: req.body.title,
-        password: req.body.title,
-      })
+    let salt = bcrypt.genSaltSync(saltRounds);
+    let hash = bcrypt.hashSync(req.body.password, salt);
+    return User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: hash
+    })
       .then(user => res.status(201).send(user))
       .catch(error => res.status(400).send(error));
-  },
+  }
 };
+
+
+// Work on login here or create another file.
